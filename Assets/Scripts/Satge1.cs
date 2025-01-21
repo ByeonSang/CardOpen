416aeb4ad5a885b808526523afaf3fd95dba9f9c
JUNG99 <kinggodjung9@naver.com>
2025-01-21 14:22:14 +0900

    stage1

---
+++ Assets/Scripts/Satge1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Satge1 : MonoBehaviour
+{
+   public void Stage1()
+    {
+        SceneManager.LoadScene("MainScene");
+            
+            }
+}
